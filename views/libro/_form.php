b57d983a41ae3233db6c1270839ef6edb83a66e6
ste3711 <fernando.franco.rivadeneira@utelvt.edu.ec>
2023-09-30 21:48:13 -0500

    Actualizacion datos

--- views/libro/_form.php
+++ views/libro/_form.php
@@ -2,46 +2,69 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
 /** @var yii\web\View $this */
 /** @var app\models\Libro $model */
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="libro-form">
 
-    <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'codigo_barra')->textInput(['maxlength' => true]) ?>
+<div class="row">
+    <div class="col-md-6">
+        <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'n_ejemplares_libro')->textInput() ?>
+        <?= $form->field($model, 'codigo_barra')->label('Código de Barra')->textInput() ?>
 
-    <?= $form->field($model, 'num')->textInput() ?>
+        <?= $form->field($model, 'n_ejemplares_libro')->label('N° Ejemplares de Libros')->textInput() ?>
 
-    <?= $form->field($model, 'lib_asignatura')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'num')->label('Número')->textInput() ?>
 
-    <?= $form->field($model, 'lib_isbn')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'lib_asignatura')->label('Tipo de Asignatura')->dropDownList([
+            'Biología' => 'Biología',
+            'Bioquímica' => 'Bioquímica',
+            'Economía' => 'Deporte',
+            'Educación Física' => 'Educación Física',
+            'Deporte' => 'Deporte',
+            'Matemática' => 'Matemática',
+            'Mecánica' => 'Mecánica',
+            'Medio Ambiente' => 'Medio Ambiente',
+        ], ['prompt' => 'Selecciona una asignatura']) ?>
 
-    <?= $form->field($model, 'lib_nombre')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'lib_isbn')->label('ISBN')->textInput() ?>
+        <?= $form->field($model, 'lib_nombre')->label('Nombre del Libro')->textInput() ?>
 
-    <?= $form->field($model, 'lib_categoria')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'lib_categoria')->label('Categoría')->dropDownList([
+            'Libro' => 'Libro',
+            'Diccionario' => 'Diccionario',
+            'Revista' => 'Revista',
+        ], ['prompt' => 'Selecciona una categoria']) ?>
 
-    <?= $form->field($model, 'lib_autor')->textInput(['maxlength' => true]) ?>
+        </div>
+    <div class="col-md-6">
+        <?= $form->field($model, 'lib_autor')->label('Autor del Libro')->textInput() ?>
 
-    <?= $form->field($model, 'lib_editorial')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'lib_editorial')->label('Editorial')->textInput() ?>
 
-    <?= $form->field($model, 'lib_pais')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'lib_pais')->label('País')->dropDownList([
+            'EC' => 'EC',
+            'CO' => 'CO',
+            'ES' => 'ES',
+        ], ['prompt' => 'Selecciona un Pais']) ?>
 
-    <?= $form->field($model, 'lib_anio')->textInput() ?>
+        <?= $form->field($model, 'lib_anio')->label('Año de Publicación')->textInput()->input('date') ?>
 
-    <?= $form->field($model, 'lib_duplicado')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'lib_duplicado')->label('Duplicado')->input('number') ?>
 
-    <?= $form->field($model, 'lib_estado')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'lib_estado')->label('Estado')->dropDownList([
+            'Disponible' => 'Disponible',
+            'Prestado' => 'Prestado',
+        ], ['prompt' => 'Selecciona un estado']) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <br>
+        <div class="col-md-2 float-end">
+            <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
--- views/site/login.php
+++ views/site/login.php
@@ -1,5 +1,4 @@
 <?php
-
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
 
@@ -12,44 +11,51 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-lg-5">
-
-            <?php $form = ActiveForm::begin([
-                'id' => 'login-form',
-                'fieldConfig' => [
-                    'template' => "{label}\n{input}\n{error}",
-                    'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-                    'inputOptions' => ['class' => 'col-lg-3 form-control'],
-                    'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-                ],
-            ]); ?>
-
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-            <?= $form->field($model, 'password')->passwordInput() ?>
-
-            <?= $form->field($model, 'rememberMe')->checkbox([
-                'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            ]) ?>
-
-            <div class="form-group">
-                <div>
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            </div>
-
-            <?php ActiveForm::end(); ?>
-
-            <div style="color:#999;">
-                You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-                To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-            </div>
-
-        </div>
+            <div class="login-container p-4 border rounded shadow-lg"> <!-- Nuevo contenedor -->
+            <div class="text-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+        </svg>
     </div>
+            <h1 class="text-center display-6" style="font-family: 'Open Sans', sans-serif;">Iniciar Sesion</h1>
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'layout' => 'horizontal',
+                    'fieldConfig' => [
+                        'template' => "{label}\n{input}\n{error}",
+                        'labelOptions' => ['class' => 'col-lg-2 col-form-label'],
+                        'inputOptions' => ['class' => 'form-control'],
+                        'errorOptions' => ['class' => 'invalid-feedback'],
+                    ],
+                ]); ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => 'Ingresa tu usuario', 'class' => 'form-control'])->label('<svg xmlns="http://www.w3.org/2000/svg" width="25" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+                </svg> Usuario', ['class' => 'fw-bold']) ?>
+
+                <?= $form->field($model, 'password')->passwordInput(['type' => 'password', 'placeholder' => 'Ingresa tu Contraseña', 'class' => 'form-control'])->label(' <svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+                </svg> Contraseña', ['class' => 'fw-bold']) ?>
+                
+                <?= $form->field($model, 'rememberMe')->checkbox([
+                    'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                ]) ?>
+<div class="form-group text-center">
+    <div class="col-lg-12">
+        
+        <?= Html::submitButton('
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
+        <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+        </svg> Acceder', ['class' => 'btn btn-primary btn-block btn-lg w-100 animate_animated animate_fadeInLeft', 'name' => 'login-button']) ?>
+        <!-- Agregado w-100 para que el botón ocupe todo el ancho -->
+        <!-- Agregado animate_animated animate_fadeInLeft para añadir animación de entrada -->
+    </div>
+</div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
 </div>
