e748de485ff8f08da3afa5d37f3be24ecc956f4b
alfr0718 <69158931+alfr0718@users.noreply.github.com>
2024-02-03 19:04:35 -0500

    Update view.php

--- views/carrera/view.php
+++ views/carrera/view.php
@@ -14,67 +14,106 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="carrera-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row">
+        <!-- Primer card a la izquierda -->
+        <div class="col-md-6">
 
-    <p>
-        <?= Html::a('Actualizar', ['update', 'idcar' => $model->idcar], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Eliminar', ['delete', 'idcar' => $model->idcar], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => '¿Estas seguro de Eliminar este elemento?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+            <div class="card shadow mb-4">
+                <!-- Card Header - Accordion -->
+                <div class="card-header py-3 text-center">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <div>
+                            <h1 class="m-0 font-weight-bold text-primary">
+                                <?= Html::encode($this->title) ?>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'idcar',
-            'Nombre',
-            [
-                'attribute' => 'Status',
-                'value' => function ($model) {
-                    return $model->Status == 1 ? 'Activo' : 'Inactivo';
-                },
-            ],
-        ],
-    ]) ?>
-    
-    <?= Html::a('Agregar Asignatura', ['agregar-asignatura-carrera', 'idcar' => $model->idcar], ['class' => 'btn btn-primary']) ?>
 
-    <?php $contador = 1;
-    foreach ($AllAsignaturas as $Asignatura) : ?>
-        <div class="row">
-            <div class="col-md-6">
-                <?= DetailView::widget([
-                    'model' => $Asignatura,
-                    'attributes' => [
-                        [
-                            'label' => $contador,
-                            'value' => $Asignatura->Nombre,
+
+                <!-- Card Content - Collapse -->
+                <div class="card-body">
+
+
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'idcar',
+                            'Nombre',
+                            [
+                                'attribute' => 'Status',
+                                'value' => function ($model) {
+                                    return $model->Status == 1 ? 'Activo' : 'Inactivo';
+                                },
+                            ],
                         ],
-                        // Agrega más atributos según sea necesario
-                    ],
-                ]); ?>
-            </div>
-            <div class="col-md-6">
-                <?= Html::a('Editar', ['asignatura/update', 'id' => $Asignatura->id], ['class' => 'btn btn-primary']) ?>
-                <?= Html::a('Eliminar', ['asignatura/delete', 'id' => $Asignatura->id], [
-                    'class' => 'btn btn-danger',
-                    'data' => [
-                        'confirm' => '¿Estás seguro de que quieres eliminar esta asignatura?',
-                        'method' => 'post',
-                    ],
-                ]) ?>
+                    ]) ?>
+                    <p>
+                        <?= Html::a('<span class="icon text-white-100"><i class="fas fa-edit"></i></span><span class="text">Actualizar Carrera</span>', ['update', 'idcar' => $model->idcar], ['class' => 'btn btn-primary btn-icon-split']) ?>
+                        <?= Html::a('<span class="icon text-white-100"><i class="fas fa-trash"></i></span><span class="text">Eliminar Carrera</span>', ['delete', 'idcar' => $model->idcar], [
+                            'class' => 'btn btn-danger btn-icon-split',
+                            'data' => [
+                                'confirm' => '¿Estas seguro de Eliminar este elemento?',
+                                'method' => 'post',
+                            ],
+                        ]) ?>
+                    </p>
+                </div>
+
             </div>
         </div>
-    <?php
-        $contador++;
-    endforeach; ?>
 
+        <!-- Segundo card a la derecha -->
+        <div class="col-md-6">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Accordion -->
+                <div class="card-header py-3 text-center">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <div>
+                            <h3 class="text-primary">
+                                <?php echo 'Asignaturas'; ?>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p>
+                        <?= Html::a('<span class="icon text-white-100"><i class="fas fa-plus-circle"></i></span><span class="text">Añadir Asignatura</span>', ['agregar-asignatura-carrera', 'idcar' => $model->idcar], ['class' => 'btn btn-success btn-icon-split']) ?>
+                    </p>
+                    <?php $contador = 1;
+                    foreach ($AllAsignaturas as $Asignatura) : ?>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <?= DetailView::widget([
+                                    'model' => $Asignatura,
+                                    'attributes' => [
+                                        [
+                                            'label' => $contador,
+                                            'value' => $Asignatura->Nombre,
+                                        ],
+                                        // Agrega más atributos según sea necesario
+                                        [
+                                            'format' => 'raw',
+                                            'label' => '',
+                                            'value' => Html::a('<i class="fa fa-edit"></i>', ['asignatura/update', 'id' => $Asignatura->id], ['class' => 'btn btn-primary btn-circle']) .
+                                            Html::a('<i class="fa fa-trash"></i>', ['asignatura/delete', 'id' => $Asignatura->id], [
+                                                'class' => 'btn btn-danger btn-circle',
+                                                'data' => [
+                                                    'confirm' => '¿Estás seguro de que quieres eliminar esta asignatura?',
+                                                    'method' => 'post',
+                                                ],
+                                            ]),
+                                        ],
+                                    ],
+                                ]); ?>
+                            </div>
 
-
-
-
+                        </div>
+                    <?php $contador++;
+                    endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
